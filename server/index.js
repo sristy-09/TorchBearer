5e1a3f119427cfda4c2808c70509ff763a9b439f
sristy-09 <dhakalshristi09@gmail.com>
2026-03-03 08:30:55 +0545

    (backend):Initial backend setup is done

---
+++ server/index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+import { connectDB } from "./config/db.js";
+
+connectDB();
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+// app.use("/space", board);
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
