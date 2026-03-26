2d884935541939fac77c554672fb9727d11731ed
sristy-09 <dhakalshristi09@gmail.com>
2026-03-26 11:29:56 +0545

    feat(search-filter):Added apiFunctionality file which contains search and filter functionality

---
+++ server/utils/apiFunctionality.js
@@ -0,0 +1,29 @@
+class APIFunctionality {
+  constructor(query, queryStr) {
+    this.query = query;
+    this.queryStr = queryStr;
+  }
+
+  search() {
+    const keyword = this.queryStr.keyword
+      ? {
+          name: {
+            $regex: this.queryStr.keyword,
+            $options: "i",
+          },
+        }
+      : {};
+    this.query = this.query.find({ ...keyword });
+    return this;
+  }
+
+  filter() {
+    const queryCopy = { ...this.queryStr };
+
+    const removeFields = ["keyword", "page", "limit"];
+    removeFields.forEach((key) => delete queryCopy[key]);
+    this.query = this.query.find(queryCopy);
+    return this;
+  }
+}
+export default APIFunctionality;
