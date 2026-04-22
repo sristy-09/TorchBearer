500ea0ba82622d9fb4e6aa7c9fc74e04959895f0
Tuhaan <tuhaanthapa@gnail.com>
2026-04-22 10:57:48 +0545

    ui(landing-page):updated landing page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,8 @@
 import LandingPage from '@/pages/components/LandingPage'
-import { useSelector, useDispatch } from "react-redux";
-import type {RootState, AppDispatch} from "@/store"
-import { completeOnboarding } from "./store/onboardingSlice";
-import Onboarding from '@/pages/components/Onboarding'
-import Dashboard from "@/pages/components/Dashboard";
-export default function App () {
-  const dispatch = useDispatch<AppDispatch>()
-  const completed = useSelector((s: RootState) => s.onboarding.completed) 
-  const handleOnboardingComplete = ()=> {
-    dispatch(completeOnboarding())
-  }
 
-  if (!completed) {
-    return <Onboarding onComplete={handleOnboardingComplete} />
-  }
-  if (completed)
-  {return <Dashboard />}
+
+export default function App () {
+
   
   return <LandingPage />;
   
